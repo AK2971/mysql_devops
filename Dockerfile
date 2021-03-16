fb0ec4586a01bd42b9998a8d1c0633d2aaea1402
AK2971 <80771626+AK2971@users.noreply.github.com>
2021-03-16 20:50:08 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM mysql:latest
+ENV MYSQL_DATABASE pucsdStudents
+ENV MYSQL_USER pucsd
+ENV MYSQL_PASSWORD pucsd
+ADD test.sql /docker-entrypoint-initdb.d
+EXPOSE 3636
---
+++ test.sql
@@ -0,0 +1,16 @@
+use pucsdStudents;
+
+create table studentData (Roll_No int not null, Name char (20), Address char (30), Mobile_No int (10), PAN_no varchar(10), PRIMARY KEY(Roll_No));
+
+insert into studentData values  ('101', 'Monica','S.B Road', '9912345111', 'ABC1IB1234');
+
+insert into studentData values  ('102', 'Rachel','FC Road', '9912346222', 'ABC1IB1235');
+
+insert into studentData values  ('103', 'Phoebe','Karve Road', '9912347333', 'ABC1IB1236');
+
+insert into studentData values  ('104', 'Joey','Baner', '9912341444', 'ABC1IB1237');
+
+insert into studentData values  ('105', 'Chandler','Baner', '9912348555', 'ABC1IB1238');
+
+insert into studentData values  ('106', 'Ross','Aundh', '9912349666', 'ABC1IB1239');
+
